16a1c985a2407b67e22ac94c0c5744e1151e8606
Qc1998 <854375588@qq.com>
2018-04-02 21:37:04 +0800

    init Django settings.py

--- SOSCON/SOSCON/settings.py
+++ SOSCON/SOSCON/settings.py
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
@@ -118,3 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'all_static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'common_static'),
+]
