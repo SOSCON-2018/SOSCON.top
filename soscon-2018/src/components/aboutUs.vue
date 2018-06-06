81758bb1f0029588c6cc4133330ada8481d7fde7
rnguzi <bleachhddh@gmail.com>
2018-06-06 19:47:36 +0800

    picture update 1

--- soscon-2018/src/components/aboutUs.vue
+++ soscon-2018/src/components/aboutUs.vue
@@ -138,7 +138,7 @@
       </div>
       <div id="modal14" class="modal">
         <div class="modal-content">
-          <img src="../../static/img/head.jpg" class="headpic" align="right" hspace="5" vspace="5">
+          <img src="../../static/img/shao1.png" class="headpic" align="right" hspace="5" vspace="5">
           <p style="vertical-align: top;font-size: x-large;font-weight: 700">{{$t("AboutUs.about4")}}</p>
           <p style="vertical-align: top;font-size: x-large;font-weight: 500">邵明琦</p>
           <p style="vertical-align: top;font-size: x-large;font-weight: 500">Email: qichen0906@gmail.com</p>
@@ -162,7 +162,7 @@
       </div>
       <div id="modal17" class="modal">
         <div class="modal-content">
-          <img src="../../static/img/head.jpg" class="headpic" align="right" hspace="5" vspace="5">
+          <img src="../../static/img/jia.png" class="headpic" align="right" hspace="5" vspace="5">
           <p style="vertical-align: top;font-size: x-large;font-weight: 700">{{$t("AboutUs.about4")}}</p>
           <p style="vertical-align: top;font-size: x-large;font-weight: 500">贾峥</p>
           <p style="vertical-align: top;font-size: x-large;font-weight: 500">Email: bleachhddh@gmail.com</p>
@@ -321,7 +321,7 @@
           <div class="col s12 m6 l3" style="margin-top: 3%" @click="open14">
             <div class="card" style="width: 225px;height: 256px;">
               <div class="card-image" style="height: 220px;">
-                <img src="../../static/img/head.jpg">
+                <img src="../../static/img/shao.png">
               </div>
               <div class="card-action" style="background-color:#282828;">
                 <a style="color: white;margin: auto">{{$t("AboutUs.about4")}} 邵明琦</a>
@@ -354,7 +354,7 @@
           <div class="col s12 m6 l3" style="margin-top: 3%" @click="open17">
             <div class="card" style="width: 225px;height: 256px;">
               <div class="card-image" style="height: 220px;">
-                <img src="../../static/img/head.jpg">
+                <img src="../../static/img/jia1.png">
               </div>
               <div class="card-action" style="background-color:#282828">
                 <a style="color: white">{{$t("AboutUs.about4")}} 贾峥</a>
